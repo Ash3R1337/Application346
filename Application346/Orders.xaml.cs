4dd4a880b8cc6bae37d0587c2491359e2380cc64
Kseo <Mich9771@yandex.ru>
2023-01-14 10:45:14 +0300

    АХУЕТЬ

--- Application346/Orders.xaml.cs
+++ Application346/Orders.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Application346
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для Orders.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Orders : Window
     {
-        public MainWindow()
+        public Orders()
         {
             InitializeComponent();
-        }
-
-        private void OrderBtn_Click(object sender, RoutedEventArgs e)
-        {
-            Orders orders = new Orders();
-            orders.Show();
-            this.Hide();
         }
     }
 }
