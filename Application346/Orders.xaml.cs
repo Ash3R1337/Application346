09df8595e30534adc4afb43b47c23f90c529ecdc
Ash3R1337 <54432714+Ash3R1337@users.noreply.github.com>
2023-01-16 01:05:12 +0300

    Поиск по клиентамс и отображение часов в CB

--- Application346/Orders.xaml.cs
+++ Application346/Orders.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using AHlibrary;
 
 namespace Application346
 {
@@ -19,9 +20,12 @@ namespace Application346
     /// </summary>
     public partial class Orders : Window
     {
+        DBconnect dBconnect = new DBconnect();
         public Orders()
         {
             InitializeComponent();
+            dBconnect.DB("watches", "клиенты", ClientsDataGrid, "root");
+            dBconnect.FillCombobox(WatchesNameComboBox, "name", "часы");
         }
 
         private void BackBtn_Click(object sender, RoutedEventArgs e)
@@ -29,6 +33,19 @@ namespace Application346
             MainWindow mw = new MainWindow();
             mw.Show();
             this.Close();
+        }
+
+        private void ClientFoundBtn_Click(object sender, RoutedEventArgs e)
+        {
+            dBconnect.TableSearch(ClientsDataGrid, ClientFoundTextBox.Text);
+            if (ClientsDataGrid.Items.Count < 2)
+                MessageBox.Show("Клиент не найден! Добавьте нового клиента.");
+        }
+
+        private void ClientSavedBtn_Click(object sender, RoutedEventArgs e)
+        {
+            dBconnect.SaveTable();
+            MessageBox.Show("Новый клиент успешно добавлен");
         }
     }
 }
