1d3a7e1418fa3d929f83288291ab4ee3125e07fe
Ash3R1337 <54432714+Ash3R1337@users.noreply.github.com>
2023-01-15 18:38:56 +0300

    Небольшие правки

--- Application346/Tables.xaml.cs
+++ Application346/Tables.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using AHlibrary;
 
 namespace Application346
 {
@@ -29,6 +30,12 @@ namespace Application346
             MainWindow mw = new MainWindow();
             mw.Show();
             this.Close();
+        }
+
+        private void TablesOpenBtn_Click(object sender, RoutedEventArgs e)
+        {
+            DBconnect dBconnect = new DBconnect();
+            dBconnect.DB(SelectTablesComboBox.Text, ClientsDataGrid, "root");
         }
     }
 }
